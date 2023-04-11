03c6d285367945c17ca83fb705141e1e7ce34e31
danielname <daniel.v.name@gmail.com>
2023-04-11 01:36:31 -0500

    New Code - Lesson - Printing - Exercise 1 Complete

--- Lessons/Printing.py
+++ Lessons/Printing.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+#you should run your program and it should print the following:
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
