93e88505bdddd7c423851eca054a395cdfd8f406
danielname <daniel.v.name@gmail.com>
2023-04-11 17:44:09 -0500

    New Code - Lesson - Data Types - Completed Exercise 2-1

---
+++ Lessons/Data_Types.py
@@ -0,0 +1,23 @@
+# Primitive Data Types
+#  integer
+23131
+
+# float
+89437621.541635
+
+# boolean
+print(True or False)
+
+# data types
+# string
+print("hello"[2])
+
+print("123" + str(8916432)+ "345")
+
+print(float("439876219"))
+
+# Exercise 2-1
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+user_input_number = input("Please type a 2 digit number: ")
+print(int(user_input_number[0]) + int(user_input_number[1]))
