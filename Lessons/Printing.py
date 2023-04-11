c66fd961b2343d7b1c68c3ca45ff60b810794ed2
danielname <daniel.v.name@gmail.com>
2023-04-11 02:00:31 -0500

    New Code - Lesson - Printing - Completed Exercise 1-2

--- Lessons/Printing.py
+++ Lessons/Printing.py
@@ -7,3 +7,19 @@ print("Hello World")
 # print('what to print')
 
 print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+# concatonation
+
+print("hello" + " " + "Daniel")
+
+
+# When you run your program, it should print the following:
+#
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.\ne.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
