df4da4f49d0255327d3a5cff95774e73b393d3e0
danielname <daniel.v.name@gmail.com>
2023-04-11 02:18:10 -0500

    New Code - Lesson - Printing - Completed Exercise 1-3

--- Lessons/Printing.py
+++ Lessons/Printing.py
@@ -1,5 +1,16 @@
+# hello world
 print("Hello World")
 
+# concatonation
+
+print("hello" + " " + "Daniel")
+
+# inputs
+print(input("what is your name?"))
+
+
+
+#Exercise 1-1
 #you should run your program and it should print the following:
 
 # Day 1 - Python Print Function
@@ -8,11 +19,10 @@ print("Hello World")
 
 print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
 
-# concatonation
-
-print("hello" + " " + "Daniel")
 
 
+
+# Exercise 1-2
 # When you run your program, it should print the following:
 #
 # Day 1 - String Manipulation
@@ -23,3 +33,9 @@ print("hello" + " " + "Daniel")
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.\ne.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+
+# Exercise 1-3
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+print(len(input("What is your name? ")))
