1720e58eb84e8991a2b8b0473722e765327338ae
danielname <daniel.v.name@gmail.com>
2023-04-11 19:43:28 -0500

    FIX - Project - Tip Calculator - Added .format to Ensure That There Are Always 2 Decimal Places

--- Projects/TipCalculator/Tip_Calculator.py
+++ Projects/TipCalculator/Tip_Calculator.py
@@ -1,6 +1,7 @@
-total = float(input("Welcome to the tip calculator.\nWhat was the total bill? "))
+total = float(input("Welcome to the tip calculator.\nWhat was the total bill? $"))
 tip = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
 split = float(input("How many people to split the bill? "))
 cost = total * ((100+tip)/100) / split
 rounded_up = round(-((cost * 100)//-1) /100, 2) #this line makes it so that the calculation always rounds up so that you do not underpay the bill -(a//-b) is a de-facto ceiling function. alternatively I could have imported math and used math.ceil
-print(f"Each person should pay: {rounded_up}")
+formatted_bill = "{:.2f}".format(rounded_up) #this line makes sure that there are are always 2 decimal places
+print(f"Each person should pay: ${formatted_bill}")
