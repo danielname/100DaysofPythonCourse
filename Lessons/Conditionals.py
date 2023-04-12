4ccf181e2f09f237137211e6d7391d259fb63ffb
danielname <daniel.v.name@gmail.com>
2023-04-12 11:48:01 -0500

    New Code - Lesson - Conditionals - Completed Exercise - 3-2

--- Lessons/Conditionals.py
+++ Lessons/Conditionals.py
@@ -13,6 +13,14 @@ else:
 height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the roller coaster!")
+    age = int(input("How old are you? "))
+    # nested condition
+    if age < 12:
+        print("Your ticket costs $5.")
+    elif age <= 18:
+        print("Your ticket costs $7.")
+    else:
+        print("Your ticket costs $12.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
@@ -24,3 +32,26 @@ if your_number % 2 == 0:
     print("Your Number is even!")
 else:
     print("Your number is odd!")
+
+
+# Exercise 3-2
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmi = round(float(weight)/float(height)**2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
