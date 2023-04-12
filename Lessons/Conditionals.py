e738241e2324f514b2d1b0c7a213beed696f5db4
danielname <daniel.v.name@gmail.com>
2023-04-12 11:12:37 -0500

    Lesson - Control Flow (if/else)

---
+++ Lessons/Conditionals.py
@@ -0,0 +1,17 @@
+# control flow
+# if condition:
+#     do this
+# else:
+#     do this
+
+water_level = 50
+if water_level > 80:
+    print("Drain Water")
+else:
+    print("Continue")
+
+height = int(input("What is your height in cm? "))
+if height >= 120:
+    print("You can ride the roller coaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
