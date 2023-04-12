5777e0dcd95fbbe8050f037130a5bd8e1aa5b28a
danielname <daniel.v.name@gmail.com>
2023-04-12 11:20:14 -0500

    New Code - Lesson - Conditionals - Completed Exercise 3-1

--- Lessons/Conditionals.py
+++ Lessons/Conditionals.py
@@ -15,3 +15,12 @@ if height >= 120:
     print("You can ride the roller coaster!")
 else:
     print("Sorry, you have to grow taller before you can ride.")
+
+# Exercise 3-1
+# Write a program that works out whether if a given number is an odd or even number.
+
+your_number = int(input("Please type in a number: "))
+if your_number % 2 == 0:
+    print("Your Number is even!")
+else:
+    print("Your number is odd!")
