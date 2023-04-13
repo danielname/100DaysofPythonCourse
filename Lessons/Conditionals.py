586a59d01512a261e910c07286eed25999ef8963
danielname <daniel.v.name@gmail.com>
2023-04-13 15:07:30 -0500

    New Code - Lesson - Conditionals - Exercise 3-3 Complete

--- Lessons/Conditionals.py
+++ Lessons/Conditionals.py
@@ -55,3 +55,21 @@ elif bmi < 35:
     print(f"Your BMI is {bmi}, you are obese.")
 else:
     print(f"Your BMI is {bmi}, you are clinically obese.")
+
+# Exercise 3-3
+# Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February.
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
