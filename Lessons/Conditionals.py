ab45c5b06d283b959e7745593a88b5bf7aa69a6c
danielname <daniel.v.name@gmail.com>
2023-04-13 15:33:24 -0500

    New Code - Lesson - Conditionals - Completed Exercise 3-4

--- Lessons/Conditionals.py
+++ Lessons/Conditionals.py
@@ -82,3 +82,48 @@ if year % 4 == 0:
         print("Leap year.")
 else:
     print("Not leap year.")
+
+
+# Exercise 3-4
+# Congratulations, you've got a job at Python Pizza. Your first job is to build an automatic pizza order program.
+#
+# Based on a user's order, work out their final bill.
+# Small Pizza: $15
+#
+# Medium Pizza: $20
+#
+# Large Pizza: $25
+#
+# Pepperoni for Small Pizza: +$2
+#
+# Pepperoni for Medium or Large Pizza: +$3
+#
+# Extra cheese for any size pizza: + $1
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+cost = 0
+if size == "S":
+    cost += 15
+    if add_pepperoni == "Y":
+        cost += 2
+    if extra_cheese == "Y":
+        cost += 1
+elif size == "M":
+    cost += 20
+    if add_pepperoni == "Y":
+        cost += 3
+    if extra_cheese == "Y":
+        cost += 1
+else:
+    cost += 25
+    if add_pepperoni == "Y":
+        cost += 3
+    if extra_cheese == "Y":
+        cost += 1
+print(f"Your final bill is: ${cost}.")
