eecc3a905016a284403ebbf07ff75af422261170
danielname <daniel.v.name@gmail.com>
2023-04-15 01:58:38 -0500

    Lesson - Lists

---
+++ Lessons/Lists.py
@@ -0,0 +1,10 @@
+# Lists
+fruits = ["apple", "pear", "cherry"]
+print(fruits[1])
+print(fruits[-1])
+fruits[0] = "lettuce"
+print(fruits[0])
+fruits.append("orange")
+print(fruits)
+fruits.extend(["melon", "raspberry"])
+print(fruits)
