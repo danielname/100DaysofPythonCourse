3ef05366d00ce1a76e1725a3f0515bbd3c061ef8
danielname <daniel.v.name@gmail.com>
2023-04-15 11:46:31 -0500

    New Code - Lesson - Lists - Completed Exercise 4-2

--- Lessons/Lists.py
+++ Lessons/Lists.py
@@ -8,3 +8,16 @@ fruits.append("orange")
 print(fruits)
 fruits.extend(["melon", "raspberry"])
 print(fruits)
+
+# Exercise
+# You are going to write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+buyer = names[int(random.random() * len(names))]
+print(f"{buyer} is going to buy the meal today!")
