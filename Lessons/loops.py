71542ac23e6c0ca2024cbe4b25e3b875f0bf5005
danielname <daniel.v.name@gmail.com>
2023-04-16 11:39:49 -0500

    New Code - Lesson - Loops - Completed Exercise 5-4

--- Lessons/loops.py
+++ Lessons/loops.py
@@ -45,3 +45,15 @@ sum = 0
 for number in range(0,101,2):
     sum += number
 print(sum)
+
+# Exercise 5-4
+# fizzbuzz
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
