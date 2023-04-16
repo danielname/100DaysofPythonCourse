689a4d63f5dca9e964206adaca6178989af9d9cf
danielname <daniel.v.name@gmail.com>
2023-04-16 09:52:40 -0500

    New Code - Lesson - Loops - Completed Exercise 5-2

--- Lessons/loops.py
+++ Lessons/loops.py
@@ -19,3 +19,19 @@ sum = 0
 for number in student_heights:
     sum += number
 print(round(sum / len(student_heights)))
+
+# Exercise 5-2
+# You are going to write a program that calculates the highest score from a List of scores.
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+high_score = 0
+for value in student_scores:
+    if value > high_score:
+        high_score = value
+print(f"The highest score in the class is: {high_score}")
