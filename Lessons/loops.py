1aa72e7523dc8a44598854e3a55977108084012f
danielname <daniel.v.name@gmail.com>
2023-04-16 09:44:21 -0500

    New Code - Lesson - Loops - Completed Exercise 5-1

--- Lessons/loops.py
+++ Lessons/loops.py
@@ -3,3 +3,19 @@ fruits = ["apple", "peach", "pear"]
 for fruit in fruits:
     print(fruit)
     print(fruit + " pie")
+
+# Exercise 5-1
+# You are going to write a program that calculates the average student height from a List of heights.
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+for number in student_heights:
+    sum += number
+print(round(sum / len(student_heights)))
