3b92592a725d1bda3b671b7aa4648be8872aee12
danielname <daniel.v.name@gmail.com>
2023-04-17 11:23:26 -0500

    Lesson - Functions - Defining and Calling a Function

---
+++ Lessons/Functions.py
@@ -0,0 +1,10 @@
+print("hello")
+# print is a function because it is called with the parentheses
+
+# in python we define functions with def
+def my_function():
+    print("hello")
+    print("bye")
+
+# calling a function works the same as in JS
+my_function()
