79b6f7403b41b3afe1e5fae4faafee75b40dbfb5
danielname <daniel.v.name@gmail.com>
2023-04-18 14:02:50 -0500

    New Code - Project - Hangman - Challenge 1 Complete. Random Word Selected, User Asked For Guess, Guess Checked Against Word

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -18,3 +18,20 @@
 #         you lose
 #     else:
 #         return to line 6
+
+
+#Step 1
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = word_list[random.randint(0, len(word_list) - 1)]
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Please guess a letter: ").lower()
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+chosen_word.split(" ")
+for letter in chosen_word:
+    if guess == letter:
+        print("Right")
+    else:
+        print("Wrong")
