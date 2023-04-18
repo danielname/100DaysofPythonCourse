48c9a37343e157de8c9ddf6e6a594e052d13ae1c
danielname <daniel.v.name@gmail.com>
2023-04-18 13:52:33 -0500

    Pseudocode - Project - Hangman - Flocharted Hangman

---
+++ Projects/Hangman/Hangman.py
@@ -0,0 +1,20 @@
+# Setup
+# Word randomly selected
+# Make blanks for each letter in word
+# variable with the number of wrong answers till lose defined
+
+# Ask user to guess letter
+# if right:
+#     letter replaces all blanks where that letter should go
+#     letter marked off of bank
+#     check if all letters have been guessed
+#     if yes:
+#         congrats, you win
+#     if no:
+#         return to line 6
+# if wrong:
+#     variable -= 1
+#     if variable == 0:
+#         you lose
+#     else:
+#         return to line 6
