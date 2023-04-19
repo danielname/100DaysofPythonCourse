986ef5f74a612c69ef2698bd9420ee195645a8f0
danielname <daniel.v.name@gmail.com>
2023-04-19 17:32:24 -0500

    New Code - Project - Hangman - Challenge 4 Complete. Keep Track of Remaining Lives and Player Can Lose

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -18,13 +18,69 @@
 #         you lose
 #     else:
 #         return to line 6
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 
-#Step 3
+#Step 4
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
+lives = 6
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -34,15 +90,22 @@ for letter in chosen_word:
     display.append('_')
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-while display.count("_") > 0:
+while display.count("_") > 0 and lives > 0:
     guess = input("Please guess a letter: ").lower()
-
-    # check guessed letter
-    for position in range(len(chosen_word)):
-        if chosen_word[position] == guess:
-            print("Right")
-            display[position] = guess
-        else:
-            print("Wrong")
-
-    print(display)
+    if guess in chosen_word:
+        # check guessed letter
+        for position in range(len(chosen_word)):
+            if chosen_word[position] == guess:
+                print("Right")
+                display[position] = guess
+            else:
+                print("Wrong")
+    else:
+        print("Sorry, that letter is not in the word")
+        lives -= 1
+        print(stages[lives])
+    print(f"{' '.join(display)}")
+if display.count("_") == 0:
+    print("Congratulations! You win!")
+else:
+    print("Sorry, you lose.")
