2c2043df0e5c46fbf099d16105a4c2b8b5581d72
danielname <daniel.v.name@gmail.com>
2023-04-21 13:02:09 -0500

    FIX - Project - Caesar Cipher - Cleaned Up Caesar Method

--- Projects/CaesarCipher/Caesar_Cipher.py
+++ Projects/CaesarCipher/Caesar_Cipher.py
@@ -5,18 +5,17 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def caesar(text_input, shift_amount, encode_or_decode):
-    if encode_or_decode == "encode":
-        cipher_text = ""
-        for letter in text_input:
-            if alphabet.index(letter) + shift_amount < len(alphabet):
-                cipher_text += alphabet[alphabet.index(letter) + shift_amount]
+    new_text = ""
+    for letter in text_input:
+        letter_position = alphabet.index(letter)
+        if encode_or_decode == "decode":
+            new_text += alphabet[letter_position - shift_amount]
+        elif encode_or_decode == "encode":
+            if letter_position + shift_amount < len(alphabet):
+                new_text += alphabet[letter_position + shift_amount]
             else:
-                cipher_text += alphabet[-(len(alphabet) - alphabet.index(letter) - shift_amount)]
-        print(cipher_text)
-    elif encode_or_decode == "decode":
-        plain_text = ""
-        for letter in text_input:
-            plain_text += alphabet[alphabet.index(letter) - shift_amount]
-        print(plain_text)
+                new_text += alphabet[-(len(alphabet) - letter_position - shift_amount)]
+    print(new_text)
+
 
 caesar(text, shift, direction)
