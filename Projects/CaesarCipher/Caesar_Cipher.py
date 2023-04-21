3874c6a2aaae7d75b86b230bddfe74b149d02fa7
danielname <daniel.v.name@gmail.com>
2023-04-21 11:15:06 -0500

    New Code - Project - Caesar Cipher - Decryption Complete

--- Projects/CaesarCipher/Caesar_Cipher.py
+++ Projects/CaesarCipher/Caesar_Cipher.py
@@ -4,7 +4,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
@@ -13,17 +12,14 @@ def encrypt(plain_text, shift_amount):
         else:
             cipher_text += alphabet[-(len(alphabet) - alphabet.index(letter) - shift)]
     print(cipher_text)
-#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-#e.g.
-#plain_text = "hello"
-#shift = 5
-#cipher_text = "mjqqt"
-#print output: "The encoded text is mjqqt"
 
-##HINT: How do you get the index of an item in a list:
-#https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        plain_text += alphabet[alphabet.index(letter) - shift]
+    print(plain_text)
 
-##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-encrypt(text,shift)
+if direction == "encript":
+    encrypt(text,shift)
+else:
+    decrypt(text,shift)
