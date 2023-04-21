f831dc9fa0b093471c37642baa02df65b179720c
danielname <daniel.v.name@gmail.com>
2023-04-21 13:19:14 -0500

    New Code - Project - Caesar Cipher - Cleaned Up Function and Added ASCII Art

--- Projects/CaesarCipher/Caesar_Cipher.py
+++ Projects/CaesarCipher/Caesar_Cipher.py
@@ -1,3 +1,5 @@
+import art
+print(art.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -7,14 +9,9 @@ shift = int(input("Type the shift number:\n"))
 def caesar(text_input, shift_amount, encode_or_decode):
     new_text = ""
     for letter in text_input:
-        letter_position = alphabet.index(letter)
         if encode_or_decode == "decode":
-            new_text += alphabet[letter_position - shift_amount]
-        elif encode_or_decode == "encode":
-            if letter_position + shift_amount < len(alphabet):
-                new_text += alphabet[letter_position + shift_amount]
-            else:
-                new_text += alphabet[-(len(alphabet) - letter_position - shift_amount)]
+            shift_amount *= -1
+        new_text += alphabet[(alphabet.index(letter) + shift_amount) % len(alphabet)]
     print(new_text)
 
 
