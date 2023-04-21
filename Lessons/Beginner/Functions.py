430df2e93fc06fcccd0c1d49d11e98249f4f639f
danielname <daniel.v.name@gmail.com>
2023-04-21 08:49:25 -0500

    New Code - Lesson - Functions - Completed Exercise 8-1

--- Lessons/Beginner/Functions.py
+++ Lessons/Beginner/Functions.py
@@ -26,3 +26,27 @@ greet_with("Joe", "home")
 
 # can call functions with keywords
 greet_with(location="away", name="Sam")
+
+
+# Exercise 8-1
+# You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
+#Write your code below this line 👇
+
+def paint_calc(height, width, cover = 5):
+    paint_needed = (height * width) / cover
+    rounded = round(paint_needed)
+    if rounded >= paint_needed:
+        print(f"You'll need {rounded} cans of paint.")
+    else:
+        print(f"You'll need {rounded + 1} cans of paint.")
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
