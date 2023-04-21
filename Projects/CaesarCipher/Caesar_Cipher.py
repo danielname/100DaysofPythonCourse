dd068b6ff80e174e740a1c42788b029f201b194e
danielname <daniel.v.name@gmail.com>
2023-04-21 14:34:11 -0500

    New Code - Project - Caesar Cipher - Project Complete

--- Projects/CaesarCipher/Caesar_Cipher.py
+++ Projects/CaesarCipher/Caesar_Cipher.py
@@ -8,10 +8,13 @@ shift = int(input("Type the shift number:\n"))
 
 def caesar(text_input, shift_amount, encode_or_decode):
     new_text = ""
-    if encode_or_decode == "decode": #there is currently something wrong with decode I think the issue is on line 14
+    if encode_or_decode == "decode":
         shift_amount *= -1
     for letter in text_input:
-        new_text += alphabet[(alphabet.index(letter) + shift_amount) % len(alphabet)]
+        if letter in alphabet:
+            new_text += alphabet[(alphabet.index(letter) + shift_amount) % len(alphabet)]
+        else:
+            new_text += letter
     result = input(f"{new_text}\nWould you like to encript or decript another message? yes/no\n")
     if result == "yes":
         direction_new = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -21,12 +24,3 @@ def caesar(text_input, shift_amount, encode_or_decode):
 
 caesar(text, shift, direction)
 
-#TODO-3: What happens if the user enters a number/symbol/space?
-#Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-#e.g. start_text = "meet me at 3"
-#end_text = "•••• •• •• 3"
-
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
