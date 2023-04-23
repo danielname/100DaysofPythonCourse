b2c9fda07467d22e9518c675d533135d08800939
danielname <daniel.v.name@gmail.com>
2023-04-23 11:40:06 -0500

    Syntax Change - Lesson - Dictionaries - Updated Solution for 9-2 to Make Function More Readable

--- Lessons/Beginner/Dictionaries.py
+++ Lessons/Beginner/Dictionaries.py
@@ -110,7 +110,11 @@ travel_log = [
 #TODO: Write the function that will allow new countries
 #to be added to the travel_log. 👇
 def add_new_country(country, visits, cities):
-    travel_log.append({"country": country, "visits": visits, "cities": cities})
+    new_country = {}
+    new_country["country"] = country
+    new_country["visits"] = visits
+    new_country["cities"] = cities
+    travel_log.append(new_country)
 
 
 
