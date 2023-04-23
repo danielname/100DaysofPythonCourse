7dd920b07ad9d1185994b3f7517a7eba1ef9d3ee
danielname <daniel.v.name@gmail.com>
2023-04-23 11:35:49 -0500

    New Code - Lesson - Dictionaries - Completed Exercise 9-2

--- Lessons/Beginner/Dictionaries.py
+++ Lessons/Beginner/Dictionaries.py
@@ -89,3 +89,31 @@ for student in student_scores:
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
+
+
+# Exercise 9-2
+# You are going to write a program that adds to a travel_log. You can see a travel_log which is a List that contains 2 Dictionaries.
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+def add_new_country(country, visits, cities):
+    travel_log.append({"country": country, "visits": visits, "cities": cities})
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
