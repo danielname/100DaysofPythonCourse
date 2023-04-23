856390d9f7602b5530daf784207db95828786105
danielname <daniel.v.name@gmail.com>
2023-04-22 23:32:50 -0500

    Lesson - Dictionaries - Syntax and Definition of a Dictionary

---
+++ Lessons/Beginner/Dictionaries.py
@@ -0,0 +1,7 @@
+#similar to hash tables, we have a key value pair where we have a word and an associated definition
+programming_dictionary = {
+    "bug": "An error in a program that prevents the program from running as expected",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+print(programming_dictionary["bug"])
