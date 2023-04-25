bf245ab7964546d6b39e6982abd8fd780fcfc268
danielname <daniel.v.name@gmail.com>
2023-04-25 11:15:11 -0500

    New Code - Project - Calculator - Added Loop to Continue Calculation Off of Result

--- Projects/Calculator/Calculator.py
+++ Projects/Calculator/Calculator.py
@@ -22,9 +22,13 @@ operations = {
 
 starting_number = int(input("What's the first number?\n"))
 running = True
-symbol = input("+\n-\n*\n/\nPick an operation\n")
-second_number = int(input("What's the next number?\n"))
-function = operations[symbol]
-result = function(starting_number, second_number)
-print(f"{starting_number} {symbol} {second_number} = {result}")
-continue_calcualtion = input(f"Type 'y' to continue calculting with {result}, or type 'n' to start a new calculation.")
+while running:
+    symbol = input("+\n-\n*\n/\nPick an operation\n")
+    second_number = int(input("What's the next number?\n"))
+    function = operations[symbol]
+    result = function(starting_number, second_number)
+    print(f"{starting_number} {symbol} {second_number} = {result}")
+    starting_number = result
+    continue_calcualtion = input(f"Type 'y' to continue calculting with {result}, or type 'n' to start a new calculation.")
+    if not continue_calcualtion == "y":
+        running = False
