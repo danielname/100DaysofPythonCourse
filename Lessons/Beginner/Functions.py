b38b932f1578486b020a237d8293df2a30da1696
danielname <daniel.v.name@gmail.com>
2023-04-25 08:09:32 -0500

    Lesson - Functions - Docstrings

--- Lessons/Beginner/Functions.py
+++ Lessons/Beginner/Functions.py
@@ -45,6 +45,15 @@ def format_name(f_name, l_name):
         return "you didnt provide valid inputs"
     return f"{f_name} {l_name}".title()
 
+# Docstrings
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case version of it"""
+    if f_name == "" or l_name == "":
+        return "you didnt provide valid inputs"
+    return f"{f_name} {l_name}".title()
+
+format_name()
+
 
 # Exercise 8-1
 # You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
