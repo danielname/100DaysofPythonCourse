1e6ca2f50617fec3dc0b288e98fb2e060b0ed088
danielname <daniel.v.name@gmail.com>
2023-04-26 11:43:20 -0500

    New Code - Project - Blackjack - Setup

---
+++ Projects/Blackjack/Blackjack.py
@@ -0,0 +1,34 @@
+from art import logo
+from random import choice
+print(logo)
+
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+#Difficulty Super Expert ☠️: Dont use any hints to complete the project. <-this is my choice
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size.
+## There are no jokers.
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+player_score = 0
+player_cards = [choice(cards), choice(cards)]
+dealer_score = 0
+dealer_cards = [choice(cards)]
+
+for card in player_cards:
+    player_score += card
+    if player_score > 21 and 11 in player_cards:
