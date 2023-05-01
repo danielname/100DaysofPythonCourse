b11d89f5c00a343cf8288d2d18bb01f65b032654
danielname <daniel.v.name@gmail.com>
2023-05-01 16:14:00 -0500

    Lesson - Scope

---
+++ Lessons/Beginner/scope.py
@@ -0,0 +1,27 @@
+player_health = 10
+def drink_potion():
+    print(player_health)
+drink_potion()
+
+def drink_potion():
+    player_health = 12
+    print(player_health)
+drink_potion()
+
+enemies = "zombie"
+
+def change_enemies():
+    enemies = "skeleton"
+    print(enemies)
+print(enemies)
+
+count = 1
+def increase_count():
+    return count + 1
+higher_count = increase_count()
+
+print(higher_count)
+
+# global constants
+PI = 3.1415925
+# use all caps
