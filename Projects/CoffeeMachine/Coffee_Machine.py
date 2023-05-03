a881958169ceeb047c740c465655931bca3bc248
danielname <daniel.v.name@gmail.com>
2023-05-03 11:06:52 -0500

    New Code - Projects - Coffee Machine - Coffee Machine Functional For Report, Restock, and Espresso

--- Projects/CoffeeMachine/Coffee_Machine.py
+++ Projects/CoffeeMachine/Coffee_Machine.py
@@ -1,3 +1,5 @@
+from art import logo
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -24,9 +26,57 @@ MENU = {
     }
 }
 
+money = 0
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+COINS = {
+    "penny": 0.01,
+    "nickel": 0.05,
+    "dime": 0.1,
+    "quarter": 0.25
+}
+
+caffeinated = False
+not_enough_water = lambda: print("Sorry, there is not enough water to make your drink")
+not_enough_milk = lambda: print("Sorry, there is not enough milk to make your drink")
+not_enough_coffee = lambda: print("Sorry, there is not enough coffee to make your drink")
+
+def pay():
+    quarters = int(input("How many quarters? "))
+    dimes = int(input("How many dimes? "))
+    nickels = int(input("How many nickels? "))
+    pennies = int(input("How many pennies? "))
+    return quarters * COINS['quarter'] + dimes * COINS['dime'] + nickels * COINS['nickel'] + pennies * COINS['penny']
+
+print(logo)
+while not caffeinated:
+    user_input = input("What kind of coffee would you like? (espresso/latte/cappuccino) ")
+    if user_input == 'report':
+        print(f"Water: {resources['water']}ml\nMilk: {resources['milk']}ml\nCoffee: {resources['coffee']}g\nMoney: ${money}")
+    elif user_input == 'restock':
+        resources['water'] += int(input("How much water would you like to add in ml: "))
+        resources['milk'] += int(input("How much milk would you like to add in ml: "))
+        resources['coffee'] += int(input("How much coffee would you like to add in g: "))
+    elif user_input == 'espresso':
+        if MENU['espresso']['ingredients']['water'] > resources['water']:
+            not_enough_water()
+        if MENU['espresso']['ingredients']['coffee'] > resources['coffee']:
+            not_enough_coffee()
+        if MENU['espresso']['ingredients']['water'] <= resources['water'] and MENU['espresso']['ingredients']['coffee'] < resources['coffee']:
+            print("Your espresso will cost $1.50, please insert coins:")
+            total_pay = pay()
+            if total_pay < MENU['espresso']['cost']:
+                print("Sorry, that is not enough money. You have been refunded your coins.")
+            else:
+                resources['water'] -= MENU['espresso']['ingredients']['water']
+                resources['coffee'] -= MENU['espresso']['ingredients']['coffee']
+                money += MENU['espresso']['cost']
+                print(f"Your change is ${round(total_pay - MENU['espresso']['cost'], 2)}\nHere is your espresso ☕. Enjoy!")
+    happy = input("Would you like another coffee? y/n ").lower()
+    if happy == 'n':
+        caffeinated = True
