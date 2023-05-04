a6ba2c8e2f84f644a56df6d33c9949a5b052a75c
danielname <daniel.v.name@gmail.com>
2023-05-04 14:15:49 -0500

    FIX - Projects - Band Name Generator and Tip Calculator - Tweak for Cleanliness and Note to Improve

--- Projects/BandNameGenerator/Band_Name_Generator.py
+++ Projects/BandNameGenerator/Band_Name_Generator.py
@@ -5,5 +5,5 @@ home_town = input("What's the name of the city you grew up in?\n")
 #3. Ask the user for the name of a pet.
 pet_name = input("What's your pet's name?\n")
 #4. Combine the name of their city and pet and show them their band name.
-print("Your band name could be " + home_town + " " + pet_name)
+print(f"Your band name could be {home_town} {pet_name}")
 #5. Make sure the input cursor shows on a new line:
--- Projects/TipCalculator/Tip_Calculator.py
+++ Projects/TipCalculator/Tip_Calculator.py
@@ -5,3 +5,4 @@ cost = total * ((100+tip)/100) / split
 rounded_up = round(-((cost * 100)//-1) /100, 2) #this line makes it so that the calculation always rounds up so that you do not underpay the bill -(a//-b) is a de-facto ceiling function. alternatively I could have imported math and used math.ceil
 formatted_bill = "{:.2f}".format(rounded_up) #this line makes sure that there are are always 2 decimal places
 print(f"Each person should pay: ${formatted_bill}")
+# different ways to split check (e.g. by item, etc)
