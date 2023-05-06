3aa629d27b06e4ce4b2e1ad299f51157bccc3d51
danielname <daniel.v.name@gmail.com>
2023-05-06 17:19:18 -0500

    New Code - Project - Coffee Machine OOP - Completed Project

--- Projects/CoffeeMachineOOP/main.py
+++ Projects/CoffeeMachineOOP/main.py
@@ -1,3 +1,17 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+pot = CoffeeMaker()
+money = MoneyMachine()
+
+order = input(f"What would you like to drink? {menu.get_items()}").lower()
+if order == "report":
+    pot.report()
+    money.report()
+else:
+    drink = menu.find_drink(order)
+    if pot.is_resource_sufficient(drink):
+        if money.make_payment(drink.cost):
+            pot.make_coffee(drink)
