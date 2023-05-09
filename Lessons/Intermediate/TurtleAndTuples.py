693c601e15ee17ae8d756714970d21b336965c9e
danielname <daniel.v.name@gmail.com>
2023-05-09 16:22:30 -0500

    Lesson - Turtle

---
+++ Lessons/Intermediate/TurtleAndTuples.py
@@ -0,0 +1,23 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.forward(50)
+timmy.right(45)
+timmy.forward(25)
+timmy.left(90)
+timmy.back(75)
+timmy.left(90)
+timmy.forward(75)
+timmy.right(90)
+timmy.forward(25)
+timmy.right(45)
+timmy.forward(10)
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
