182b455f1f854d927609bb1a5ac96f179b68e79c
danielname <daniel.v.name@gmail.com>
2023-05-09 16:25:15 -0500

    New Code - Lesson - Turtle - Completed Exercise 18-1

--- Lessons/Intermediate/TurtleAndTuples.py
+++ Lessons/Intermediate/TurtleAndTuples.py
@@ -2,20 +2,23 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.forward(50)
-timmy.right(45)
-timmy.forward(25)
-timmy.left(90)
-timmy.back(75)
-timmy.left(90)
-timmy.forward(75)
-timmy.right(90)
-timmy.forward(25)
-timmy.right(45)
-timmy.forward(10)
+# timmy.forward(50)
+# timmy.right(45)
+# timmy.forward(25)
+# timmy.left(90)
+# timmy.back(75)
+# timmy.left(90)
+# timmy.forward(75)
+# timmy.right(90)
+# timmy.forward(25)
+# timmy.right(45)
+# timmy.forward(10)
 
-
-
+# Exercise 18-1
+# Draw a square
+for num in range(0,4):
+    timmy.forward(100)
+    timmy.right(90)
 
 
 
