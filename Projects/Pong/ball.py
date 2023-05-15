41d61aae278c66a0d7f2f73918878c534a18785c
danielname <daniel.v.name@gmail.com>
2023-05-15 10:57:15 -0500

    New Code - Project - Pong - Calculated and Wrote Both Bounce Functions

--- Projects/Pong/ball.py
+++ Projects/Pong/ball.py
@@ -1,12 +1,21 @@
 from turtle import Turtle
-import random
+from random import randint, choice
 
 
 class Ball(Turtle):
 
     def __init__(self):
         super().__init__("circle")
-        self.setheading(random.randint(0,359))
+        possible_directions = [randint(0,60), randint(120,179), randint(180,240), randint(300,359)]
+        self.setheading(choice(possible_directions))
         self.color("white")
         self.penup()
 
+    def wall_bounce(self):
+        self.setheading(360 - self.heading())
+
+    def paddle_bounce(self):
+        if self.heading() <= 180:
+            self.setheading(180 - self.heading())
+        else:
+            self.setheading(540 - self.heading())
