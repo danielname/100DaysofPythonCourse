ea0481e58d4671e82489189b65bb793d5a938181
danielname <daniel.v.name@gmail.com>
2023-05-25 14:59:29 -0500

    New Code - Project - Frogger - Created Player and Hop Function, Instantiated and Set Listener for Hop

--- Projects/Frogger/main.py
+++ Projects/Frogger/main.py
@@ -8,6 +8,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.hop,"Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- Projects/Frogger/player.py
+++ Projects/Frogger/player.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__("turtle")
+        self.penup()
+        self.speed(0)
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def hop(self):
+        self.fd(MOVE_DISTANCE)
+
