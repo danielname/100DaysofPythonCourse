04cfe1212a2b679bbda230a12ce597f95fac537a
danielname <daniel.v.name@gmail.com>
2023-05-29 18:45:44 -0500

    Lesson - Writing to a File

--- Lessons/Intermediate/Files/main.py
+++ Lessons/Intermediate/Files/main.py
@@ -1,5 +1,18 @@
-# Accessing a file
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# Accessing a file (basic)
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# Using with to open files (saves needing to close file)
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# Writing to a file (deletes the previous content)
+with open("my_file.txt", mode="w") as file:
+    file.write("Adding some new text")
+
+# appending to a file with mode="a"
+with open("my_file.txt", mode="a") as file:
+    file.write("Adding some new text")
