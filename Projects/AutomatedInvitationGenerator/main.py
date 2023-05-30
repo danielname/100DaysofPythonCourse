9ab6431dae9cb2d667e42a7aad7cd957e1a214ed
danielname <daniel.v.name@gmail.com>
2023-05-30 14:45:02 -0500

    New Code - Project - Automated Letter Writer - Completed Program

--- Projects/AutomatedInvitationGenerator/main.py
+++ Projects/AutomatedInvitationGenerator/main.py
@@ -1,8 +1,19 @@
 with open("./Letters/starting_letter.txt") as base:
-    base_letter = base.read()
+    base_letter = base.readlines()
 
 with open("./Names/invited_names.txt") as names:
-    name_list = names.read()
+    name_list = names.readlines()
 
-with open(f"./Output/ReadyToSend/{name_list}", mode="w") as letter:
-    letter.write()
+first_line = base_letter[0]
+
+for name in name_list:
+    name = name.replace("\n", "")
+    named_line = first_line.replace("[name]", name)
+    new_letter = []
+    for line in range(len(base_letter)):
+        if line == 0:
+            new_letter.append(named_line)
+        else:
+            new_letter.append(base_letter[line])
+    with open(f"./Output/ReadyToSend/{name}", mode="w") as letter:
+        letter.writelines(new_letter)
