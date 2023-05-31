81c9de09430943c8942ca79150ac4b866cf816eb
danielname <daniel.v.name@gmail.com>
2023-05-31 16:42:19 -0500

    Test - Checking the Use of IN for Booleans in Dictionaries

--- Lessons/Beginner/Dictionaries.py
+++ Lessons/Beginner/Dictionaries.py
@@ -3,6 +3,7 @@ programming_dictionary = {
     "bug": "An error in a program that prevents the program from running as expected",
     "Function": "A piece of code that you can easily call over and over again."
 }
+print("bug" in programming_dictionary)
 
 print(programming_dictionary["bug"])
 
