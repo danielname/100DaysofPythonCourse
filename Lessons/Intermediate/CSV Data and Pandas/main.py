5eb8b9daa088c53bdeb1aec6127f3fa62e58f015
danielname <daniel.v.name@gmail.com>
2023-06-01 09:46:13 -0500

    Lesson - CSV - Using CSV Module to Split CSV Data Into Individual Items

--- Lessons/Intermediate/CSV Data and Pandas/main.py
+++ Lessons/Intermediate/CSV Data and Pandas/main.py
@@ -1,2 +1,10 @@
-with open("weather_data.csv") as csv:
-    data = csv.readlines()
+# with open("weather_data.csv") as csv:
+#     data = csv.readlines()
+#     print(data)
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    for row in data:
+        print(row)
