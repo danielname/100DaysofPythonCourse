b984f75398b30508d7967e72d9640ea15f1a73a1
danielname <daniel.v.name@gmail.com>
2023-06-04 12:47:47 -0500

    New Code - Lesson - Pandas - Completed Squirrel Data Project/Challenge

--- Lessons/Intermediate/CSV Data and Pandas/main.py
+++ Lessons/Intermediate/CSV Data and Pandas/main.py
@@ -14,45 +14,61 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(type(data)) #this is a dataframe
 # print(type(data["temp"])) #this is a series
 
 # data_dict = data.to_dict()
 # print(data_dict)
 
-temp_list = data["temp"].to_list()
-# channlenge: claculate the average temp in this time period
-print(sum(temp_list) / len(temp_list))
-print(temp_list)
-print(data["temp"].mean())
+# temp_list = data["temp"].to_list()
+# # channlenge: claculate the average temp in this time period
+# print(sum(temp_list) / len(temp_list))
+# print(temp_list)
+# print(data["temp"].mean())
+#
+# print(data["temp"].max())
+#
+# # accessing data in column
+# print(data["condition"])
+# print(data.condition)
+#
+# # accessing data in row
+# print(data[data.day == "Monday"])
+#
+# # challenge - pull data from row when temp was max
+# print(data[data.temp == data.temp.max()])
+#
+# # accessing specific datum in a row
+# monday = data[data.day == "Monday"]
+# print(monday.condition)
+#
+# print(int(monday.temp) * 9 / 5 + 32)
+#
+# # create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# new_data = pandas.DataFrame(data_dict)
+# print(new_data)
+#
+# # creating a new csv file
+# new_data.to_csv("new_data.csv")
 
-print(data["temp"].max())
-
-# accessing data in column
-print(data["condition"])
-print(data.condition)
-
-# accessing data in row
-print(data[data.day == "Monday"])
-
-# challenge - pull data from row when temp was max
-print(data[data.temp == data.temp.max()])
-
-# accessing specific datum in a row
-monday = data[data.day == "Monday"]
-print(monday.condition)
-
-print(int(monday.temp) * 9 / 5 + 32)
-
-# create a dataframe from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+squirrel_fur = {
+    "color": ["gray", "cinnamon", "black"],
+    "count": [0, 0, 0]
 }
 
-new_data = pandas.DataFrame(data_dict)
-print(new_data)
+squirrel_data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray = squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"].count().X
+squirrel_fur["count"][0] = gray
+cinnamon = squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"].count().X
+squirrel_fur["count"][1] = cinnamon
+black = squirrel_data[squirrel_data["Primary Fur Color"] == "Black"].count().X
+squirrel_fur["count"][2] = black
 
-# creating a new csv file
-new_data.to_csv("new_data.csv")
+squirrel_csv = pandas.DataFrame(squirrel_fur)
+squirrel_csv.to_csv("squirrel_count.csv")
