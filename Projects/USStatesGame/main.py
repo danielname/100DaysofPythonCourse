42332c2df7dcc19821e5c063e63e42e9a3f89caf
danielname <daniel.v.name@gmail.com>
2023-06-08 21:27:20 -0500

    New Code - Project - US States Game - Read States Data and Created Position Variables

--- Projects/USStatesGame/main.py
+++ Projects/USStatesGame/main.py
@@ -8,5 +8,12 @@ screen.addshape(image)
 
 turtle.shape(image)
 
+states_data = pandas.read_csv("50_states.csv")
 
 state_guess = screen.textinput("Guess a state", "Name another state: ")
+
+x = 0
+y = 0
+
+# if state_guess in states_data.state:
+
