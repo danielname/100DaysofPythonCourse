1235bf18e9594880570d26ff6054e684972bb5a6
danielname <daniel.v.name@gmail.com>
2023-06-09 23:18:05 -0500

    New Code - Project - US States Game - Made Writer and Write Condition (not yet functional)

--- Projects/USStatesGame/main.py
+++ Projects/USStatesGame/main.py
@@ -8,6 +8,10 @@ screen.addshape(image)
 
 turtle.shape(image)
 
+writer = turtle.Turtle(visible=False)
+writer.speed(0)
+writer.penup()
+
 states_data = pandas.read_csv("50_states.csv")
 
 state_guess = screen.textinput("Guess a state", "Name another state: ")
@@ -15,5 +19,10 @@ state_guess = screen.textinput("Guess a state", "Name another state: ")
 x = 0
 y = 0
 
-# if state_guess in states_data.state:
+if state_guess in states_data.state:
+    x = states_data.state.state_guess.x
+    y = states_data.state.state_guess.y
+    writer.goto(x,y)
+    writer.write(state_guess, align="center")
 
+screen.exitonclick()
