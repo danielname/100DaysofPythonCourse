8bf5728e0c066557f60ec3bbed959edcdf1e54b9
danielname <daniel.v.name@gmail.com>
2023-06-13 17:56:28 -0500

    New Code - Project - Treasure Island - Spruced Up the Adventure

--- Projects/TreasureIsland/Treasure_Island.py
+++ Projects/TreasureIsland/Treasure_Island.py
@@ -21,21 +21,27 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.")
+print('It is a nice day! When you wake up, you get the mail and receive a letter from your weird, pirate uncle, who claims there is a lost treasure on a deserted island near your home.')
 
-#Write your code below this line 👇
-direction = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"\n')
-if direction.lower() == "left":
-    lake = input('You came to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
-    if lake.lower() == "wait":
-        house = input("You arrive at the island unharmed. There is a house with 3 doors: one red, one yellow, and one blue. Which color door do you choose to go through?\n")
-        if house.lower() == "yellow":
-            print("You found a fantastic treasure! Congratulations, you win!")
-        elif house.lower() == "blue":
-            print("You released wild beasts that tore you to sheds and ate your corpse. GAME OVER!")
+# alive = True
+
+first_step = input('To take up the challenge of finding the treasure, type "adventure." To stay home and play video games type "stay."')
+# while alive:
+if first_step.lower() == "adventure" or first_step.lower() == "adventure.":
+    direction = input('You\'re at a cross road. You feel a salty breeze coming from the left, dry warmth coming from the right. Where do you want to go? Type "left" or "right"\n')
+    if direction.lower() == "left":
+        lake = input('You came to a lake. You don\'t see anything unusual, but the water is a bit choppy. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+        if lake.lower() == "wait":
+            house = input("You arrive at the island unharmed. There is a house with 3 glazed glass doors: one shines red, one yellow, and one blue. Which color door do you choose to go through?\n")
+            if house.lower() == "yellow":
+                print("You found a fantastic treasure! Congratulations, you win!")
+            elif house.lower() == "blue":
+                print("You released wild beasts that tore you to sheds and ate your corpse. GAME OVER!")
+            else:
+                print("You were trapped in the house, which caught ablaze! You perish in the fire! GAME OVER!")
         else:
-            print("You were trapped in the house, which caught ablaze! You perish in the fire! GAME OVER!")
+            print("You were attacked by a trout and drowned. GAME OVER!")
     else:
-        print("You were attacked by a trout and died. GAME OVER!")
+        print("You find yourself in a desert. You fell in a hole and dehydrated to death. GAME OVER!")
 else:
-    print("You fell in a hole and died. GAME OVER!")
+    print("You stay and live an uneventful life. You die sad and alone. GAME OVER!")
