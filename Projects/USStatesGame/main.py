fb936d7888d897c1b604e839724a0579ab869a44
danielname <daniel.v.name@gmail.com>
2023-06-17 08:51:46 -0500

    Refactor - Project - US States Game - Created States List to Allow Further Functionality

--- Projects/USStatesGame/main.py
+++ Projects/USStatesGame/main.py
@@ -13,6 +13,7 @@ pen = writer.Writer()
 
 
 states_data = pandas.read_csv("50_states.csv")
+state_list = states_data["state"].to_list()
 
 state_guess = screen.textinput("Guess a state", "Name another state: ").title()
 
@@ -20,7 +21,7 @@ game = True
 while game:
     if state_guess == "Exit":
         break
-    if states_data[states_data.state == state_guess].notnull:
+    if state_guess in state_list:
         pen.x = states_data[states_data.state == state_guess]["x"].max()
         pen.y = states_data[states_data.state == state_guess]["y"].max()
         pen.write_state(state_guess)
