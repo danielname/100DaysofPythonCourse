ea78526b0cd346f89ef33047138e692b8b6148b6
danielname <daniel.v.name@gmail.com>
2023-06-22 21:22:29 -0500

    New Code - Lesson - List Comprehension - Exercise 26-1 Complete

--- Lessons/Intermediate/ListComprehension/main.py
+++ Lessons/Intermediate/ListComprehension/main.py
@@ -23,3 +23,19 @@ print(short_names)
 # challenge
 long_upper_names = [listname.upper() for listname in names if len(listname) > 4]
 print(long_upper_names)
+
+# Exercise 26-1
+#You are going to write a List Comprehension to create a new list called squared_numbers. This new list should contain every number in the list numbers but each number should be squared.
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+
+#Write your 1 line code 👇 below:
+
+squared_numbers = [n * n for n in numbers]
+
+#Write your code 👆 above:
+
+print(squared_numbers)
+
+
