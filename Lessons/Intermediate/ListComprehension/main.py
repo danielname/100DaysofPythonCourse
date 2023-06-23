704e623a28dc9141fa367d6207294a5fdf7ad1e8
danielname <daniel.v.name@gmail.com>
2023-06-22 21:24:59 -0500

    Refactor - Lesson - List Comprehension - Used Power Operator Instead Of Multiplying By Same for Better Updatability

--- Lessons/Intermediate/ListComprehension/main.py
+++ Lessons/Intermediate/ListComprehension/main.py
@@ -32,7 +32,7 @@ numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 #Write your 1 line code 👇 below:
 
-squared_numbers = [n * n for n in numbers]
+squared_numbers = [n ** 2 for n in numbers]
 
 #Write your code 👆 above:
 
