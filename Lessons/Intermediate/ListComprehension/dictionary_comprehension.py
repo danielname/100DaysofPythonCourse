d1643dc026641aaaed4dc7c39eb18cda2366c2c0
danielname <daniel.v.name@gmail.com>
2023-06-25 09:40:16 -0500

    New Code - Lesson List Comprehension - Completed Exercise 26-4

--- Lessons/Intermediate/ListComprehension/dictionary_comprehension.py
+++ Lessons/Intermediate/ListComprehension/dictionary_comprehension.py
@@ -7,3 +7,19 @@ student_scores = {student: random.randint(1, 100) for student in names}
 passing_students = {student: score for (student, score) in student_scores.items() if score >= 60}
 
 print(passing_students)
+
+# Exercise 26-4
+#You are going to use Dictionary Comprehension to create a dictionary called result that takes each word in the given sentence and calculates the number of letters in each word.
+#
+# Do NOT Create a dictionary directly. Try to use Dictionary Comprehension instead of a Loop.
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+
+sentence_list = sentence.split(" ")
+
+result = {word: len(word) for word in sentence_list}
+
+print(result)
