4daa4347134a2c181792d2a8c0cb2803831658a8
danielname <daniel.v.name@gmail.com>
2023-06-27 18:48:01 -0500

    New Code - Project - NATO Alphabet - Completed Project

---
+++ Projects/NATOAlphabet/main.py
@@ -0,0 +1,33 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {}
+for (index, row) in nato_data.iterrows():
+    nato_dict[row.letter] = row.code
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+in_word = input("What word would you like a phonetic spelling of?")
+nato_list = [letter for letter in in_word.upper()]
+for letter in nato_list:
+    print(nato_dict[letter])
