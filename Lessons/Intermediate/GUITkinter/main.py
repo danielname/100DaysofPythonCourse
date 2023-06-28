f81780341994de03941fc434b8d3e788fc3726ca
danielname <daniel.v.name@gmail.com>
2023-06-28 18:50:42 -0500

    Lesson - tkinter - Characteristics and Pack

--- Lessons/Intermediate/GUITkinter/main.py
+++ Lessons/Intermediate/GUITkinter/main.py
@@ -1,12 +1,13 @@
 import tkinter
 
 window = tkinter.Tk()
+# characteristic parameters
+window.title("First GUI Program")
+window.minsize(width=450, height=300)
 
-
-
-
-
-
+# Labels
+my_label = tkinter.Label(text="I am a label", font=("Arial", 12, "normal"))
+my_label.pack()
 
 
 
