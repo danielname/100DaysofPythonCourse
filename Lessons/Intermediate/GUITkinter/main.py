7bf4604741b215a80380c3b0b7f65ba6b509ad5a
danielname <daniel.v.name@gmail.com>
2023-06-30 20:44:49 -0500

    Lesson - Tkinter - Config

--- Lessons/Intermediate/GUITkinter/main.py
+++ Lessons/Intermediate/GUITkinter/main.py
@@ -7,9 +7,10 @@ window.minsize(width=450, height=300)
 
 # Labels
 my_label = tkinter.Label(text="I am a label", font=("Arial", 12, "normal"))
-my_label.pack(side="left ")
+my_label.pack(side="left")
 
-
+# my_label["text"] = "Not a label"
+my_label.config(text="Not a label")
 
 
 
