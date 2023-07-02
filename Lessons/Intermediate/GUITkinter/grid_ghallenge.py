4c280378d6fe2811c0f7067d5d46ee104643e8ae
danielname <daniel.v.name@gmail.com>
2023-07-02 07:48:18 -0500

    Refactor - Tkinter - 0 Indexed Grid Challenge

--- Lessons/Intermediate/GUITkinter/grid_ghallenge.py
+++ Lessons/Intermediate/GUITkinter/grid_ghallenge.py
@@ -4,21 +4,21 @@ window = Tk()
 window.title("Grid Challenge")
 window.minsize(width=500, height=500)
 
-# label at 1,1
+# label at 0,0
 my_label = Label(text="On top left")
-my_label.grid(column=1, row=1)
+my_label.grid(column=0, row=0)
 
-# button at 2,2
+# button at 1,1
 first_button = Button(text="down right from label")
-first_button.grid(column=2, row=2)
+first_button.grid(column=1, row=1)
 
-# button at 3,1
+# button at 2,0
 second_button = Button(text="up right from button")
-second_button.grid(column=3, row=1)
+second_button.grid(column=2, row=0)
 
 
-# entry at 4,3
+# entry at 3,2
 my_entry = Entry()
-my_entry.grid(column=4, row=3)
+my_entry.grid(column=3, row=2)
 
 window.mainloop()
