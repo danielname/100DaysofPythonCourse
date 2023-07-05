29487ec189dc8e7e420f5fd10f92622631e6348c
danielname <daniel.v.name@gmail.com>
2023-07-04 23:25:44 -0500

    New Code - Project - Unit Converter - Started Working Toward Multiple Versions

--- Projects/UnitConverter/main.py
+++ Projects/UnitConverter/main.py
@@ -6,10 +6,15 @@ MI_TO_KM = 1.609344
 def calculate():
     conversion.config(text=str(float(num_box.get()) * MI_TO_KM))
 
+def radio_used():
+#     config all the things
 
 window = Tk()
 window.config(padx=20, pady=20)
 window.minsize(width=300, height=200)
+
+radio_state = IntVar()
+conv_select = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
 
 unit_1 = Label(text="Miles")
 unit_1.grid(column=2, row=0)
