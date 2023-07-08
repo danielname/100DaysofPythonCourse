9951d88b26c212cc31d3d0e27ced560122a46982
danielname <daniel.v.name@gmail.com>
2023-07-07 22:12:24 -0500

    New Code - Project - Unit Converter - Added Volume Conversions

--- Projects/UnitConverter/main.py
+++ Projects/UnitConverter/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 MI_TO_KM = 1.609344
 KM_TO_MI = 0.62137119
+GAL_TO_LIT = 3.78541178
+LIT_TO_GAL = 0.26417205
 
 
 def calculate_km_mi():
@@ -15,6 +17,12 @@ def calculate_f_c():
 
 def calculate_c_f():
     conversion.config(text=str(float(num_box.get()) * 9/5 + 32))
+
+def calculate_gal_lit():
+    conversion.config(text=str(float(num_box.get()) * GAL_TO_LIT))
+
+def calculate_lit_gal():
+    conversion.config(text=str(float(num_box.get()) * LIT_TO_GAL))
 
 def radio_used_1():
     unit_1.config(text="Miles")
@@ -40,6 +48,18 @@ def radio_used_4():
     conversion.config(text="0")
     calc_btn.config(command=calculate_c_f)
 
+def radio_used_5():
+    unit_1.config(text="Gallons")
+    unit_2.config(text="Liters")
+    conversion.config(text="0")
+    calc_btn.config(command=calculate_gal_lit)
+
+def radio_used_6():
+    unit_1.config(text="Liters")
+    unit_2.config(text="Gallons")
+    conversion.config(text="0")
+    calc_btn.config(command=calculate_lit_gal)
+
 window = Tk()
 window.config(padx=20, pady=20)
 window.minsize(width=300, height=200)
@@ -49,10 +69,15 @@ mi_km_select = Radiobutton(text="mi to km", value=1, variable=radio_state, comma
 km_mi_select = Radiobutton(text="km to mi", value=2, variable=radio_state, command=radio_used_2)
 f_c_select = Radiobutton(text="fahrenheit to celsius", value=3, variable=radio_state, command=radio_used_3)
 c_f_select = Radiobutton(text="celsius to fahrenheit", value=4, variable=radio_state, command=radio_used_4)
+gal_lit_select = Radiobutton(text="gallons to liters", value=5, variable=radio_state, command=radio_used_5)
+lit_gal_select = Radiobutton(text="liters to gallons", value=6, variable=radio_state, command=radio_used_6)
+
 mi_km_select.grid(column=3, row=0)
 km_mi_select.grid(column=3, row=1)
 f_c_select.grid(column=3, row=2)
 c_f_select.grid(column=3, row=3)
+gal_lit_select.grid(column=3, row=4)
+lit_gal_select.grid(column=3, row=5)
 
 unit_1 = Label(text="Miles")
 unit_1.grid(column=2, row=0)
