1dcacca2999600570f5352c7929169ef8b8da2c1
danielname <daniel.v.name@gmail.com>
2023-07-09 09:34:33 -0500

    Lesson - Setup Tkinter window

--- Projects/PomodoroTimer/main.py
+++ Projects/PomodoroTimer/main.py
@@ -1,9 +1,9 @@
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+BLUE = "#00235b"
+RED = "#e21818"
+GREEN = "#98dfd6"
+YELLOW = "#ffdd83"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -18,6 +18,13 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Timer")
+window.config(pady=50, padx=100, bg=BLUE)
+
+canvas = Canvas(width=200, height=224, bg=BLUE, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100,112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
 
 
 window.mainloop()
