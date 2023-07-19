a5ac1d560b2f4e001fbcdc9797956ef74b1994b4
danielname <daniel.v.name@gmail.com>
2023-07-18 19:06:23 -0500

    New Code - Project - Password Manager - Tweaked Layout and Focused Curser on Website

--- Projects/PasswordManager/main.py
+++ Projects/PasswordManager/main.py
@@ -7,7 +7,7 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(pady=20, padx=20, width=240, height=240)
+window.config(pady=50, padx=50, width=240, height=240)
 
 lock_canvas = Canvas(width=200, height=200, highlightthickness=0)
 lock_image = PhotoImage(file="logo.png")
@@ -17,22 +17,24 @@ lock_canvas.grid(column=1, row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=35)
+website_input = Entry(width=35, justify="left")
 website_input.grid(column=1, columnspan=2, row=1)
+website_input.focus()
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 
-email_input = Entry(width=35)
+email_input = Entry(width=35, justify="left")
 email_input.grid(column=1, columnspan=2, row=2)
 
 pw_label = Label(text="Password:")
 pw_label.grid(column=0, row=3)
 
-pw_input = Entry(width=21)
+pw_input = Entry(width=21, justify="left")
 pw_input.grid(column=1, row=3)
 
-pw_button = Button(text="Generate Password")
+pw_button = Button(text="Generate Password", justify="left")
+pw_button.config(padx=0)
 pw_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=36)
