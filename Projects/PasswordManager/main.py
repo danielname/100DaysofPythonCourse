f61a69dea170fee88d3afad6bbc75dd38e3a2b6a
danielname <daniel.v.name@gmail.com>
2023-07-26 23:43:57 -0500

    New Code - Project - Password Manager - Failed Attempt to Add New Password to CSV. Comment with Next Suggestion Added

--- Projects/PasswordManager/main.py
+++ Projects/PasswordManager/main.py
@@ -18,7 +18,9 @@ def add_press():
     pw_text = pw_input.get()
 
     # make dataframe
-
+    password_dict["website"] += web_text #this doesnt work. have to assign values to the next index
+    password_dict["email"] += email_text
+    password_dict["password"] += pw_text
 
     password_csv = pandas.DataFrame(passwrord_dict)
     password_csv.to_csv("password_list.csv")
