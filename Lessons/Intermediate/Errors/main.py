1298cf713c13f907bfc9b71e3d10a2a8b811853f
danielname <daniel.v.name@gmail.com>
2023-08-06 22:22:34 -0500

    Lesson - Errors - Types of Errors

---
+++ Lessons/Intermediate/Errors/main.py
@@ -0,0 +1,15 @@
+# FileNotFound
+# with open("a_file.txt") as file:
+#     file.read()
+
+# KeyError
+# a_dictionary = {"key": "Value"}
+# value = a_dictionary["not_the_key"]
+
+# IndexError
+# fruit_list = ["apple", "pear", "cherry"]
+# fruit = fruit_list[3]
+
+# type error
+# text = "abc"
+# print(text + 5)
