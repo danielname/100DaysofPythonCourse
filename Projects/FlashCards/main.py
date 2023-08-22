4e1134e301c89f02ed0a963636c517248873feee
danielname <daniel.v.name@gmail.com>
2023-08-21 22:55:57 -0500

    New Code - Project - Flash Cards - Imports and Pseudocode

--- Projects/FlashCards/main.py
+++ Projects/FlashCards/main.py
@@ -1,2 +1,18 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice
+import pandas
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+#---------------next word-------------------#
+
+
+#----------------card flip------------------#
+
+
+#------------------add to list (wrong)------#
+
+
+#---------------------UI--------------------#
 
