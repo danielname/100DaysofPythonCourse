f79f653d9e0b1315e2a867b063f412517577685c
danielname <daniel.v.name@gmail.com>
2023-08-22 20:18:22 -0500

    New Code - Project - Flash Cards - Set Up Canvas and Background

--- Projects/FlashCards/main.py
+++ Projects/FlashCards/main.py
@@ -15,4 +15,13 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 
 #---------------------UI--------------------#
+window = Tk()
+window.title("Learn French")
+window.config(width=650, height=400)
 
+front_canvas = Canvas(width=650, height=400, highlightthickness=0)
+front_image = PhotoImage(file="../FlashCards/images/card_front.png")
+front_canvas.create_image(650, 400, image=front_image)
+front_canvas.grid(row=0, column=0)
+
+window.mainloop()
