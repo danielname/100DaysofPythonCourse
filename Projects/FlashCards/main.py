b61522b2ff5b8fbde24dc0ade9b8ed2e8ebe2b91
danielname <daniel.v.name@gmail.com>
2023-08-24 22:27:54 -0500

    Broken and Pseudo Code - Project - Flash Cards - Attempts at Making Background Image. Comment with Likely Solution

--- Projects/FlashCards/main.py
+++ Projects/FlashCards/main.py
@@ -5,6 +5,8 @@ import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+FRONT_BACKGROUND_IMAGE = "./images/card_front.png"
+
 #---------------next word-------------------#
 
 
@@ -19,10 +21,14 @@ window = Tk()
 window.title("Learn French")
 window.config(width=650, height=400)
 
-front_canvas = Canvas(width=650, height=400, highlightthickness=0)
-front_image = PhotoImage(file="../FlashCards/images/card_front.png")
-front_canvas.create_image(650, 400, image=front_image)
-front_canvas.grid(row=0, column=0)
+background_image = PhotoImage(...)
+background_label = Label(image="./images") #i think the problem here is that i have written the filepath, but I need an image, so the first step is to get the image from the file
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# front_canvas = Canvas(width=650, height=400, highlightthickness=0)
+# front_image = PhotoImage(file=)
+# front_canvas.create_image(650, 400, image=front_image)
+# front_canvas.grid(row=0, column=0)
 
 title = Label(text="something", justify="center")
 title.grid(row=1, column=1)
